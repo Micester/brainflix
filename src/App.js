370f00946374649c8a44bc871beabf3dd0359eef
Isaiah Mckenzie <Mr.isaiahmck@gmail.com>
2021-10-26 17:55:35 -0700

    Mobile-Header Scssing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import './App.css';
 import Header from './components/Header/Header';
 import logoSrc from './assets/Logo/BrainFlix-logo.svg';
 import avatarSrc from './assets/Images/Mohan-muruge.jpg';
