cc1adcc92f95354b2e850e39afcbee9b192d98bd
Isaiah Mckenzie <Mr.isaiahmck@gmail.com>
2021-10-29 15:11:47 -0700

    Touched up Scss for Mobile

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,19 +1,12 @@
 //Main Title Under Hero
 const Title = (props) => {
-
     console.log('Title Props', props.TitleList[0].title) //Shows VideoPlayerList Array in console
-
     const HeroTitle = props.TitleList[0].title; //Simplistic grabbing image from above Array
 
     return(
-      
         <section className="About-Title">
-  
             <h1 className="About__Title__text">{HeroTitle}</h1>
-         
         </section>
-   
     )
 }
-
 export default Title;
